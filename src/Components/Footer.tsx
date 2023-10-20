3a38e0cf9230b7a9630b387c3e2c5b6aa70eca09
MohammadIsmailAlam <ismailalam901@gmail.com>
2023-10-20 17:17:10 +0600

    footer component addded

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <p>Created by the brilliant minds behind SpaceX.</p>
+    </header>
+  );
+}
+
+export default Header;
