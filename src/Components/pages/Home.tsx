7aab26daffe1dd1c3be45ac4fdd12b1f5cace488
MohammadIsmailAlam <ismailalam901@gmail.com>
2023-10-20 17:18:06 +0600

    added footer in home page

--- src/Components/pages/Home.tsx
+++ src/Components/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LaunchList from './../LaunchList';
 import Header from '../Header';
+import Footer from '../Footer';
 function Home() {
 
   return (
     <div className="home">
         <Header/>
         <LaunchList />
+        <Footer/>
     </div>
   );
 }
